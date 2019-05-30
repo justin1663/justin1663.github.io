2c05d2d26582d6611a25ac38fbaa6047b5b8cab8
justin1663 <43288491+justin1663@users.noreply.github.com>
2019-05-30 21:17:08 +0800

    Add files via upload
    change the way of fade

--- image/joke.js
+++ image/joke.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
 	"use strict";
 
 	$(".main-image").click(function () {
-		$(".main-image").hide();
+		$(".main-image").fadeOut();
 	});
 	$(".column-img").click(function () {
 		var that = $(this).children("img").attr("src");
 		$(".main-image img").attr("src", that);
-		$(".main-image").show();
+		$(".main-image").fadeIn();
 		$(".main-image .pull-left img").css("bottom", $(".main-image .pull-left img").height() / 2);
 	});
 
